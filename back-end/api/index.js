df915799c17853b00e729bd3f900609d54ab9405
lucas rocha <73042947+R0chaa@users.noreply.github.com>
2023-11-23 12:38:57 +0000

    change for presentation

--- back-end/api/index.js
+++ back-end/api/index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:3000' // Defina a origem que você deseja permitir
+    origin: '*' // Defina a origem que você deseja permitir
 }));
 
 const port = 4000;
